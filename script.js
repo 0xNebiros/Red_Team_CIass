beb6254c3fe5c2f6efa4c9b572c48b5ec8f1d6ce
0xNebiros <78921601+0xNebiros@users.noreply.github.com>
2021-02-17 17:01:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $.getJSON('https://api.ipdata.co?api-key=0f752a112d35ee5736b1d54e74786c59613c06e7177d7770ec067418', function(data) {
   $('#ip').html(JSON.stringify(data, null, 2));
-  alert("hola");
+  
+  $.ajax({
+  	type: "POST",
+  	url: "http://localhost/index2.php",
+  	data: data,
+  	dataType: "json"
+  });
+
 });
